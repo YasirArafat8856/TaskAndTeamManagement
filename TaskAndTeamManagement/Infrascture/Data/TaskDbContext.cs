6131aee8a9e0ab175c43f90ff28b37006b718c98
yasir <yasirarafat8856@gmail.com>
2025-05-17 15:46:32 +0600

    relationship

--- TaskAndTeamManagement/Infrascture/Data/TaskDbContext.cs
+++ TaskAndTeamManagement/Infrascture/Data/TaskDbContext.cs
@@ -16,6 +16,28 @@ namespace TaskAndTeamManagement.Infrascture.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Core.Entities.Task>()
+               .HasOne(t => t.AssignedToUser)
+               .WithMany(u => u.AssignedTasks)
+               .HasForeignKey(t => t.AssignedToUserId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            // Task - CreatedByUser (Many-to-One)
+            modelBuilder.Entity<Core.Entities.Task>()
+                .HasOne(t => t.CreatedByUser)
+                .WithMany(u => u.CreatedTasks)
+                .HasForeignKey(t => t.CreatedByUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Task - Team (Many-to-One)
+            modelBuilder.Entity<Core.Entities.Task>()
+                .HasOne(t => t.Team)
+                .WithMany()
+                .HasForeignKey(t => t.TeamId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            // seed data
             modelBuilder.Entity<User>().HasData(
                 new User { Id = 1, FullName = "Admin User", Email = "admin@demo.com", Role = "Admin" },
                 new User { Id = 2, FullName = "Manager User", Email = "manager@demo.com", Role = "Manager" },
